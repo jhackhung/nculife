e05f2bb2f58b47f15656b45dce875b6ed13b6b10
Jack <hungjack1222@gmail.com>
2024-05-12 00:55:28 +0800

    Feat: Remove <style>

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="section">
+    <div class="mx-auto max-w-[800px] box-border p-0">
         <div class="flex flex-row space-x-8 h-screen items-center justify-center">
 
 
@@ -10,13 +10,16 @@
                 </button>
             </NuxtLink>
 
-            <button @click="notReady" class="rounded-sm bg-gray-300 p-12 text-2xl text-black shadow-md duration-200 hover:bg-gray-500">
+            <button @click="notReady"
+                class="rounded-sm bg-gray-300 p-12 text-2xl text-black shadow-md duration-200 hover:bg-gray-500">
                 交通
             </button>
-            <button @click="notReady" class="rounded-sm bg-gray-300 p-12 text-2xl text-black shadow-md duration-200 hover:bg-gray-500">
+            <button @click="notReady"
+                class="rounded-sm bg-gray-300 p-12 text-2xl text-black shadow-md duration-200 hover:bg-gray-500">
                 社團
             </button>
-            <button @click="notReady" class="rounded-sm bg-gray-300 p-12 text-2xl  text-black shadow-md duration-200 hover:bg-gray-500">
+            <button @click="notReady"
+                class="rounded-sm bg-gray-300 p-12 text-2xl  text-black shadow-md duration-200 hover:bg-gray-500">
                 宿舍
             </button>
 
@@ -30,29 +33,3 @@ function notReady() {
 }
 
 </script>
-
-<style>
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
-
-.section {
-    max-width: 800px;
-    margin: 0 auto;
-}
-
-h1 {
-    text-align: center;
-    margin-top: 20px;
-}
-
-img {
-    width: 200px;
-    height: 200px;
-    border-radius: 50%;
-    margin: 0 auto;
-    display: block;
-}
-</style>
