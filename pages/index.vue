83efb769ef48c0b0123e27bb8b012d3cf565cb59
Jack <hungjack1222@gmail.com>
2024-05-12 00:50:21 +0800

    Feat: Adjust link & Adjust position

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,35 @@
 <template>
     <div class="section">
-        <h1>Hello World</h1>
-        <!-- <NuxtLayout name="default">
-            hello
-        </NuxtLayout> -->
-        <div class="m-4 flex flex-col items-center justify-center space-y-4">
-            <BaseButton />
-        </div>
-        <img src="~/assets/shiba-17.jpg" alt="shiba" />
-        <div class="flex flex-row space-x-2">
+        <div class="flex flex-row space-x-8 h-screen items-center justify-center">
 
-            <button class="rounded-lg bg-zinc-700 p-4 px-6 text-sm text-white shadow-md duration-200 hover:bg-zinc-800">
-                click me
+
+            <NuxtLink to="/ncufresh">
+                <button
+                    class="rounded-sm bg-gray-300 p-12 text-2xl text-black shadow-md duration-200 hover:bg-gray-500">
+                    系所
+                </button>
+            </NuxtLink>
+
+            <button @click="notReady" class="rounded-sm bg-gray-300 p-12 text-2xl text-black shadow-md duration-200 hover:bg-gray-500">
+                交通
             </button>
-            
-            <button class="rounded-lg bg-zinc-700 p-4 px-6 text-sm text-white shadow-md duration-200 hover:bg-zinc-800">
-                <NuxtLink to="/ncufresh">NCUFresh</NuxtLink>
+            <button @click="notReady" class="rounded-sm bg-gray-300 p-12 text-2xl text-black shadow-md duration-200 hover:bg-gray-500">
+                社團
             </button>
-            
+            <button @click="notReady" class="rounded-sm bg-gray-300 p-12 text-2xl  text-black shadow-md duration-200 hover:bg-gray-500">
+                宿舍
+            </button>
+
         </div>
-       
+
     </div>
 </template>
+<script setup>
+function notReady() {
+    alert('The link is not ready')
+}
+
+</script>
 
 <style>
 * {
--- pages/ncufresh/index.vue
+++ pages/ncufresh/index.vue
@@ -1,12 +1,20 @@
 <template>
+    <router-link to="/">
+        <img src="@/assets/png/circle_arrow_left.png" alt=""
+            class="absolute bottom-24 left-4 m-0 w-16 h-16 cursor-pointer">
+    </router-link>
     <div class="flex justify-center items-center h-screen bg-white">
-        <div v-show="show" class="absolute w-[500px] h-[320px] p-6 bg-white shadow-lg">
-            <div class="absolute mt-20 ml-4 w-[8rem] h-20 p-4 bg-neutral-300 rotate-[-45deg]"></div>
-            <div class="position ml-48 w-44 h-36 p-4 bg-red-200"></div>
-            <div class="position ml-32 mt-4 w-80 h-32 p-2 text-sm text-left">
-                <p>文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述</p>
+
+        <div @click="toggleShow" v-show="show" class="fixed top-0 left-0 h-screen w-full z-100"></div>
+        <transition name="slide" mode="out-in">
+            <div v-show="show" class="absolute w-[500px] h-[320px] p-6 bg-white shadow-lg z-100">
+                <div class="absolute mt-20 ml-4 w-[8rem] h-20 p-4 bg-neutral-300 rotate-[-45deg]"></div>
+                <div class="position ml-48 w-44 h-36 p-4 bg-red-200"></div>
+                <div class="position ml-32 mt-4 w-80 h-32 p-2 text-sm text-left">
+                    <p>文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述文字敘述</p>
+                </div>
             </div>
-        </div>
+        </transition>
         <div class="grid grid-cols-2 gap-4">
             <div class="flex flex-col bg-gray-300 w-[400px] pl-4 pr-6 py-4 rounded-sm space-y-10 ml-20">
                 <div class="grid grid-cols-6 gap-4 bg-white px-4 py-4 float-left">
@@ -72,6 +80,8 @@
             </div>
         </div>
     </div>
+
+
 </template>
 <script setup>
 import { ref } from 'vue'
@@ -83,3 +93,15 @@ function toggleShow() {
 }
 
 </script>
+
+<style>
+.slide-enter-active,
+.slide-leave-active {
+    transition: transform 0.5s;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+    transform: translateY(-150px);
+}
+</style>
